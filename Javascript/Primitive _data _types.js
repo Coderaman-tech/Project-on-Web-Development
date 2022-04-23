322191ba637230832553662f60d0ae924b4d9321
Aman Kumar Shrivastava <81643753+Coderaman-tech@users.noreply.github.com>
2022-04-24 00:20:08 +0530

    Create Primitive _data _types.js

---
+++ Javascript/Primitive _data _types.js
@@ -0,0 +1,23 @@
+var a=2;
+console.log(typeof a)<<"\n";  //number
+
+a=3124124214412n;
+console.log(typeof a)<<"\n";  //bigint
+
+a=null;
+console.log(typeof a)<<"\n"; //object
+
+a=undefined
+console.log(typeof a)<<"\n";  //undefined
+
+a=true;
+console.log(typeof a)<<"\n";  ///boolean
+
+a=1243.124;
+console.log(typeof a)<<"\n";  //number
+
+a='a';
+console.log(typeof a)<<"\n";  //string
+
+a="aman";
+console.log(typeof a)<<"\n";  //string
